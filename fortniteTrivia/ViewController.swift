97994951e60cf6c59cac8d3b162b6de7e1ebd47c
ios5 <ios5@p-162mba-16.local>
2018-05-02 12:22:14 -0500

    Initial Commit

---
+++ fortniteTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fortniteTrivia
+//
+//  Created by ios5 on 4/30/18.
+//  Copyright © 2018 ios5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
