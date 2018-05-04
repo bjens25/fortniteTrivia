7eaa4a619d2a430248fd4847e08c6fb86cb77c89
ios5 <ios5@p-162mba-20.d214.org>
2018-05-04 12:16:09 -0500

    hello

--- fortniteTrivia/ViewController.swift
+++ fortniteTrivia/ViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController{
+    
+    var questionAndAnswer:questionAndAnswer!
+    
+    override func viewDidLoad(){
         super.viewDidLoad()
         
     }
---
+++ fortniteTrivia/questionAndAnswer.swift
@@ -0,0 +1,18 @@
+//
+//  questionAndAnswer.swift
+//  fortniteTrivia
+//
+//  Created by ios5 on 5/2/18.
+//  Copyright © 2018 ios5. All rights reserved.
+//
+
+import UIKit
+
+class questionAndAnswer: NSObject
+{
+var seasonQuestions = ["Was there a Season 1 Battle Pass?","What Season was the Battle Pass introduced?","What was the Season 2 Tier 100 reward?","What was the Season 3 Tier 100 reward?","What rarity is the John Wick skin?"]
+var rarityQuestions = ["What is better green or blue?","What color is legendary?","What color is uncommon?","What rarity is dark orange?(skins)","Name THREE Rarity's"]
+var materialQuestions = ["What is the strongest material?","What is the weakest material?","Name THREE building structures","What material do trees give you?","When you hit the blue circle, your damage to the object ____?"]
+var vBucksQuestions = ["How many V-Bucks is $9.99?","How many V-Bucks is the Battle Pass","What is a legal way of getting free V-Bucks within the game?","How many V-Bucks do you get from a mission in Save The World?","What is the currency in Fortnite?"]
+var battleQuestions = ["Name a close range weapon","Name a long range weapon","What does more damage: Bolt or Semi?","How much shield does a Mini Potion give you?","What is the max health that you can get with Bandages?"]
+}
