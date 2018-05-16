32ac97fa3ca8c1c02e81a3e8b7bc3bb2c734e84f
ios5 <ios5@p-162mba-16.local>
2018-05-16 11:51:08 -0500

    pdf

--- fortniteTrivia/PlayerNamesViewController2.swift
+++ fortniteTrivia/PlayerNamesViewController2.swift
@@ -8,28 +8,17 @@
 
 import UIKit
 
-class PlayerNamesViewController2: UIViewController {
-
+class PlayerNamesViewController2: UIViewController,UITextFieldDelegate {
+    @IBOutlet var tapGesture: UITapGestureRecognizer!
+    
+    @IBOutlet weak var playerNameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+var questionAndAnswer:ViewController
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tapGesture)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func dismissKeyboard() {
+        view.endEditing(true)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
